69f6f3ef76493db36b249308ab89eb73ae9ea592
Nikolay Astashenko <astashenko3@gmail.com>
2023-10-20 19:14:52 +0300

    Example005 Condition Ef-Else

---
+++ Example005_ConditionEfElse/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.OutputEncoding = System.Text.Encoding.Unicode;
+Console.InputEncoding = System.Text.Encoding.Unicode;
+Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}

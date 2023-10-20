b05cebb26e1d7da7a27c4abe0f5e24e33c136db2
Nikolay Astashenko <astashenko3@gmail.com>
2023-10-20 15:42:15 +0300

    Example 002_Hello User

---
+++ Exemple002_HelloUser/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.OutputEncoding = System.Text.Encoding.Unicode;
+Console.InputEncoding = System.Text.Encoding.Unicode;
+Console.WriteLine("Введите ваше имя ");
+string username = Console.ReadLine();
+Console.WriteLine("Привет, ");
+Console.WriteLine(username);
